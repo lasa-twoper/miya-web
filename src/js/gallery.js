82aee9e55c30fd05ba44b13a41233d34c4dbbe4c
lasataro <lasataro@gmail.com>
2014-01-24 16:11:24 +0900

    [update] Add Fashion sketch to Gallery

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,3 +1,10 @@
 $(function() {
-	$(".boxer").boxer();
+	// default
+	$(".boxer.boxer-default").boxer();
+	// fashion
+	$(".boxer.boxer-fashion").boxer({
+		formatter: function($target) {
+			return '<a href="' + $target.data("link") + '" target="_blank">&raquo;&nbsp;more...</a>';
+		}
+	});
 });
